a5f306f56f8f41aa206c6dee1d305b03ec377c63
wecanbedie <77538030+wecanbedie@users.noreply.github.com>
2022-11-27 13:43:48 +0300

    Всё доделал

---
+++ main.py
@@ -0,0 +1,57 @@
+import collections
+import datetime
+import argparse
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from pandas import read_excel
+
+
+
+def main():
+    """
+    parser = argparse.ArgumentParser(description='Ваш путь к файлу.')
+    parser.add_argument('filepath', type=str, help='Укажите ваш путь к excel-файлу.')
+    arg = parser.parse_args()
+    """
+    wineshop_foundation = 1994
+    current_date = datetime.datetime.today()
+    current_year = current_date.year
+    winery_age = current_year - wineshop_foundation
+
+
+    env = Environment(
+                loader=FileSystemLoader('.'),
+                autoescape=select_autoescape(['html', 'xml'])
+    )   
+    excel_file = read_excel("videocards.xlsx", sheet_name='Лист1', na_values=['N/A', 'NA'], keep_default_na=False)
+
+    database_videocards = excel_file.to_dict(orient='records')
+
+    menu = collections.defaultdict(list)
+    for wine in database_videocards:
+        menu[wine['Категория']].append(wine)
+        
+    template = env.get_template('template.html')
+
+
+    rendered_page = template.render(
+        winery_age=winery_age,
+        winecards=menu
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+
+    server = HTTPServer(
+                    ('0.0.0.0', 8000),
+                    SimpleHTTPRequestHandler
+                )
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
+
